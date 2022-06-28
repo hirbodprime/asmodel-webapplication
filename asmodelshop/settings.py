68566be5cd928d139453f6dc550aa147b972aeae
Hirbod <77854729+hirbodprime@users.noreply.github.com>
2022-06-28 12:04:23 +0430

    Update settings.py

--- asmodelshop/settings.py
+++ asmodelshop/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%7tybch=#4g&z(63*gqa_sr&s9tem@qyj9+%0lbvy4h$9jezl)'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
